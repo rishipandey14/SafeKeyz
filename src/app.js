e19994a8619c71d6149590270f26038c2a7ea9d4
Rishi <hopesalive1403@gmail.com>
2025-03-27 10:31:31 +0530

    connected database with the server and tested successfully

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const connectDb = require("./config/database");
 
-app.use("/test", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("Test passed successfully");
 });
 
-app.listen(7000, () => {
-  console.log("hello from server");
-});
+
+
+
+
+connectDb()
+  .then(() => {
+    console.log("Database connected Successfully");
+    app.listen(7000, () => {
+      console.log("hello from server");
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed" );
+  });
