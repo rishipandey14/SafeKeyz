f1280b277639e294ba3eb0b0d81dcb099c8a31f2
Rishi <hopesalive1403@gmail.com>
2025-04-02 09:12:04 +0530

    created /logout API

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -64,6 +64,18 @@ authRouter.post("/login", async (req, res) => {
   }
 });
 
+// Logout API
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now())
+  });
+  res.json({
+    message : "Logout Successfully"
+  });
+});
+
+
+
 
 
 
