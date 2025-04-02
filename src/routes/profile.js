3b85edd85ecb7fa2fe452bd6d80e469fc7cda906
Rishi <hopesalive1403@gmail.com>
2025-04-02 10:04:32 +0530

    created Edit Profile API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const {validateEditProfileData} = require("../utils/validation");
 
 
 
@@ -12,6 +13,27 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
   } catch (err) {
     res.status(400).json({
       error: err.message
+    });
+  }
+});
+
+// edit profile API
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if(!validateEditProfileData(req)) throw new Error("Invalid edit request");
+
+    const loggedInUser = req.user;
+    Object.keys(req.body).forEach(key => (loggedInUser[key] = req.body[key]));
+
+    loggedInUser.save();
+
+    res.json({
+      message : `${loggedInUser.firstName} , your profile updated successfully`,
+    })
+
+  } catch (err) {
+    res.json({
+      error : err.message
     });
   }
 });
