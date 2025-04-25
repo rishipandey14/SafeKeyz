59d066d51c9b091aa8bcdbf546c214e0c5f59d90
Rishi <hopesalive1403@gmail.com>
2025-04-25 15:47:08 +0530

    added cors middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const connectDb = require("./config/database");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PATCH','PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -9,14 +18,8 @@ app.use(cookieParser());
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 
-
-
-
 app.use("/", authRouter);
 app.use("/", profileRouter);
-
-
-
 
 
 connectDb()
