090ff769af49ba7ff7164f29f2264ea629c86721
Rishi <hopesalive1403@gmail.com>
2025-10-16 23:47:41 +0530

    created custom jwt token file

---
+++ src/utils/customJWTToken.js
@@ -0,0 +1,39 @@
+require("dotenv").config()
+const jwt = require("jsonwebtoken")
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
+
+/**
+ * @param {object} user - user object to encode in token
+ * @returns {string}  - signed JWT Token
+ */
+
+export const createToken = (user) => {
+    const user = this
+
+    const payload = {
+        _id : user._id,
+        emailId : user.emailId
+    };
+    try {
+        const token =  jwt.sign(payload, JWT_SECRET_KEY, {expiresIn : "2D",});
+        return token;
+    } catch (error) {
+        throw new Error("Token generation failed");
+    }
+}
+
+
+/**
+ * @param {string} token - The token to verify
+ * @returns {object | null} - decoded payload if valid, or null if invalid/expired
+ */
+
+export const validateToken = (token) => {
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET_KEY);
+        return decoded;
+    } catch (error) {
+        throw new Error(`Invalid or expired token : ${error.message}`);
+    }
+}
